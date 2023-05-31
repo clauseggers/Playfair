9424b16ba2d225c1d3c4447d129c5dac9f6f5dfa
Claus Eggers Sørensen <es@forthehearts.net>
2023-05-31 14:37:27 +0200

    Added build script that will produce the VFs using `fontmake`

---
+++ sources/makePlayfairVFs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+fontmake Playfair-2_1-Roman.glyphs -o variable --output-path /variable_ttf/PlayfairRomanVF.ttf
+fontmake Playfair-2_1-Italic.glyphs -o variable --output-path /variable_ttf/PlayfairItalicVF.ttf
+
+# EOF
