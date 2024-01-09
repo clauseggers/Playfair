3f35bc1cde54ca499bcccade53d3aca5e28dafe7
Claus Eggers Sørensen <es@forthehearts.net>
2024-01-09 11:07:25 +0100

    Rom f_iacute ligature and the open-fontgoggles-unicode script

--- Test documents/HTML/open-fontgoggles-unicode.sh
+++ Test documents/HTML/open-fontgoggles-unicode.sh
@@ -4,7 +4,21 @@
 os=$(uname -s)
 if [ "$os" != "Darwin" ]; then
   echo "This script requires FontGoggles which is only available for MacOS"
-  exit 0
+  exit 1
+fi
+
+# Exit if `fd` is not installed
+if ! command -v fd &> /dev/null
+then
+  echo "fd is required"
+  exit 1
+fi
+
+# Exit if Ripgrep `rg` is not installed
+if ! command -v rg &> /dev/null
+then
+  echo "Ripgrep is required"
+  exit 1
 fi
 
 # Check if the user provided a four-character argument with only letters and numbers
@@ -14,21 +28,48 @@ if [[ $# -ne 1 || ${#1} -ne 4 || ! "$1" =~ ^[[:alnum:]]+$ ]]; then
 fi
 
 # Store the argument in a variable
-search_term=$1
+unicode_hex=$1
 
 # Search for files in child directories with the provided argument using ripgrep
-files=$(find . -type f -name "*.gggls" -exec rg -l --null "$search_term" {} + | tr '\n' '\0')
+files=$(fd -e gggls -x rg -l --null "$unicode_hex")
 
 # Check if any matching files were found
 if [[ -z "$files" ]]; then
   echo "No matching files found."
-  exit 0
+  exit 1
 fi
 
-# Output the filenames of all the found files
-echo "$files" | tr '\0' '\n'
+# Count the number of files found and wait for user input
+number=$(echo -n "$files" | tr -dc '\0' | wc -c)
 
-# Open all the matching files using the MacOS `open` command
-echo "$files" | xargs -0 -I{} sh -c 'test -f "{}" && open "{}"'
+# Wait for user accept if the number of files is greater than ten
+if [ "$number" -gt 10 ]; then
+
+  # Display the message to the user
+  echo "$number files found, proceed? (y/n)"
+
+  # read user input
+  read -r user_input
+
+  # check if the user input is 'y' or 'y'
+  if [ "$user_input" = "y" ] || [ "$user_input" = "y" ]; then
+    echo "proceeding...\n"
+
+    # Output the filenames of all the found files
+    echo "$files" | tr '\0' '\n'
+
+    # Open all the matching files using the MacOS `open` command
+    echo "$files" | xargs -0 -I{} sh -c 'test -f "{}" && open "{}"'
+  else
+  echo "opening was aborted."
+fi
+
+else
+  # Output the filenames of all the found files
+  echo "$files" | tr '\0' '\n'
+
+  # Open all the matching files using the MacOS `open` command
+  echo "$files" | xargs -0 -I{} sh -c 'test -f "{}" && open "{}"'
+fi
 
 # EOF
